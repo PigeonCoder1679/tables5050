c46d2ace8664692a88536deba9445e5c89a17a12
Aarush <97274758+PigeonCoder1679@users.noreply.github.com>
2023-01-16 14:55:23 +0530

    Add files via upload

---
+++ tables_till_50x50.php
@@ -0,0 +1,1092 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tables till 50 x 50 | By Aarush Mehta</title>
+</head>
+<body style="background-color: white;" id="bgChange">
+    
+    <div class="container">
+    <?php
+    
+        $a=0;
+        $n=0;
+        $r=0;
+
+        while ($a <= 50) 
+        {
+            echo "<br>1 x ";
+            echo $n;
+            echo " = ";
+            echo $r;
+            
+            $a++;
+            $n++;
+            $r++;
+        }
+
+    ?>
+
+    <hr>
+
+    <?php
+    
+        $a=0;
+        $n=0;
+        $r=0;
+
+        while ($a <= 50) 
+        {
+            echo "<br>2 x ";
+            echo $n;
+            echo " = ";
+            echo $r*2;
+            
+            $a++;
+            $n++;
+            $r++;
+        }
+
+    ?>
+
+    <hr>
+
+<?php
+    
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>3 x ";
+        echo $n;
+        echo " = ";
+        echo $r*3;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+    
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>4 x ";
+        echo $n;
+        echo " = ";
+        echo $r*4;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<?php
+    
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>5 x ";
+        echo $n;
+        echo " = ";
+        echo $r*5;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>6 x ";
+        echo $n;
+        echo " = ";
+        echo $r*6;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>7 x ";
+    echo $n;
+    echo " = ";
+    echo $r*7;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>8 x ";
+    echo $n;
+    echo " = ";
+    echo $r*8;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<?php
+    
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>9 x ";
+        echo $n;
+        echo " = ";
+        echo $r*9;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>10 x ";
+        echo $n;
+        echo " = ";
+        echo $r*10;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>11 x ";
+    echo $n;
+    echo " = ";
+    echo $r*11;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>12 x ";
+    echo $n;
+    echo " = ";
+    echo $r*12;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+    
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>13 x ";
+        echo $n;
+        echo " = ";
+        echo $r*13;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>14 x ";
+        echo $n;
+        echo " = ";
+        echo $r*14;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>15 x ";
+    echo $n;
+    echo " = ";
+    echo $r*15;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>16 x ";
+    echo $n;
+    echo " = ";
+    echo $r*16;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>17 x ";
+    echo $n;
+    echo " = ";
+    echo $r*17;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>18 x ";
+    echo $n;
+    echo " = ";
+    echo $r*18;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>19 x ";
+echo $n;
+echo " = ";
+echo $r*19;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>20 x ";
+echo $n;
+echo " = ";
+echo $r*20;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>21 x ";
+    echo $n;
+    echo " = ";
+    echo $r*21;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>22 x ";
+    echo $n;
+    echo " = ";
+    echo $r*22;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>23 x ";
+echo $n;
+echo " = ";
+echo $r*23;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>24 x ";
+echo $n;
+echo " = ";
+echo $r*24;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<?php
+    
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>25 x ";
+        echo $n;
+        echo " = ";
+        echo $r*25;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>26 x ";
+        echo $n;
+        echo " = ";
+        echo $r*26;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>27 x ";
+    echo $n;
+    echo " = ";
+    echo $r*27;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>28 x ";
+    echo $n;
+    echo " = ";
+    echo $r*28;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>29 x ";
+    echo $n;
+    echo " = ";
+    echo $r*29;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>30 x ";
+    echo $n;
+    echo " = ";
+    echo $r*30;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>31 x ";
+echo $n;
+echo " = ";
+echo $r*31;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>32 x ";
+echo $n;
+echo " = ";
+echo $r*32;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>33 x ";
+    echo $n;
+    echo " = ";
+    echo $r*33;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>34 x ";
+    echo $n;
+    echo " = ";
+    echo $r*34;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>35 x ";
+echo $n;
+echo " = ";
+echo $r*35;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>36 x ";
+echo $n;
+echo " = ";
+echo $r*36;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<?php
+    
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>37 x ";
+        echo $n;
+        echo " = ";
+        echo $r*37;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+
+    $a=0;
+    $n=0;
+    $r=0;
+
+    while ($a <= 50) 
+    {
+        echo "<br>38 x ";
+        echo $n;
+        echo " = ";
+        echo $r*38;
+        
+        $a++;
+        $n++;
+        $r++;
+    }
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>39 x ";
+    echo $n;
+    echo " = ";
+    echo $r*39;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>40 x ";
+    echo $n;
+    echo " = ";
+    echo $r*40;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>41 x ";
+    echo $n;
+    echo " = ";
+    echo $r*41;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>42 x ";
+    echo $n;
+    echo " = ";
+    echo $r*42;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>43 x ";
+echo $n;
+echo " = ";
+echo $r*43;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>44 x ";
+echo $n;
+echo " = ";
+echo $r*44;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>45 x ";
+    echo $n;
+    echo " = ";
+    echo $r*45;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+    echo "<br>46 x ";
+    echo $n;
+    echo " = ";
+    echo $r*46;
+    
+    $a++;
+    $n++;
+    $r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>47 x ";
+echo $n;
+echo " = ";
+echo $r*47;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>48 x ";
+echo $n;
+echo " = ";
+echo $r*48;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>49 x ";
+echo $n;
+echo " = ";
+echo $r*49;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+<hr>
+
+<?php
+
+$a=0;
+$n=0;
+$r=0;
+
+while ($a <= 50) 
+{
+echo "<br>50 x ";
+echo $n;
+echo " = ";
+echo $r*50;
+
+$a++;
+$n++;
+$r++;
+}
+
+?>
+
+    </div>
+</body>
+</html>
